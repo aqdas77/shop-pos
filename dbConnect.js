b30fb9a031668f55ab3ca22bb8d35415abebef55
Mohammad Aqdas <95559517+aqdas77@users.noreply.github.com>
2022-07-01 23:27:28 +0530

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-console.log(process.env.MONGO_URL)
+
 mongoose.connect(process.env.MONGO_URL)
 let connectionObj = mongoose.connection
 connectionObj.on('connected',()=>{
